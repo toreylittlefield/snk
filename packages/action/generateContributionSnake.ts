97622148982f4017393d9f0e4421c69ac265f9b6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-20 03:21:30 -0400

    snake9 from snake4 for longer chain

--- packages/action/generateContributionSnake.ts
+++ packages/action/generateContributionSnake.ts
@@ -1,51 +1,51 @@
-import { getGithubUserContribution } from "@snk/github-user-contribution";
-import { userContributionToGrid } from "./userContributionToGrid";
-import { getBestRoute } from "@snk/solver/getBestRoute";
-import { createGif } from "@snk/gif-creator";
-import { createSvg } from "../svg-creator";
-import { snake4 } from "@snk/types/__fixtures__/snake";
-import { getPathToPose } from "@snk/solver/getPathToPose";
+import { getGithubUserContribution } from '@snk/github-user-contribution';
+import { userContributionToGrid } from './userContributionToGrid';
+import { getBestRoute } from '@snk/solver/getBestRoute';
+import { createGif } from '@snk/gif-creator';
+import { createSvg } from '../svg-creator';
+import { snake9 } from '@snk/types/__fixtures__/snake';
+import { getPathToPose } from '@snk/solver/getPathToPose';
 
 export const generateContributionSnake = async (
   userName: string,
   format: { svg?: boolean; gif?: boolean }
 ) => {
-  console.log("🎣 fetching github user contribution");
+  console.log('🎣 fetching github user contribution');
   const { cells, colorScheme } = await getGithubUserContribution(userName);
 
   const grid = userContributionToGrid(cells, colorScheme);
-  const snake = snake4;
+  const snake = snake9;
 
   const drawOptions = {
     sizeBorderRadius: 2,
     sizeCell: 16,
     sizeDot: 12,
-    colorBorder: "#1b1f230a",
+    colorBorder: '#1b1f230a',
     colorDots: colorScheme as any,
     colorEmpty: colorScheme[0],
-    colorSnake: "purple",
+    colorSnake: 'purple',
     cells,
     dark: {
-      colorEmpty: "#161b22",
-      colorDots: { 1: "#01311f", 2: "#034525", 3: "#0f6d31", 4: "#00c647" },
+      colorEmpty: '#161b22',
+      colorDots: { 1: '#01311f', 2: '#034525', 3: '#0f6d31', 4: '#00c647' },
     },
   };
 
   const gifOptions = { frameDuration: 100, step: 1 };
 
-  console.log("📡 computing best route");
+  console.log('📡 computing best route');
   const chain = getBestRoute(grid, snake)!;
   chain.push(...getPathToPose(chain.slice(-1)[0], snake)!);
 
   const output: Record<string, Buffer | string> = {};
 
   if (format.gif) {
-    console.log("📹 creating gif");
+    console.log('📹 creating gif');
     output.gif = await createGif(grid, chain, drawOptions, gifOptions);
   }
 
   if (format.svg) {
-    console.log("🖌 creating svg");
+    console.log('🖌 creating svg');
     output.svg = createSvg(grid, chain, drawOptions, gifOptions);
   }
 
